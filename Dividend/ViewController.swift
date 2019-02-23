bdb3496d7ec9852cbb0555c1d371b3ba9fc332a6
Christopher Moore <christopherkmoore1@gmail.com>
2019-02-22 17:04:56 -0800

    Initial Commit

---
+++ Dividend/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dividend
+//
+//  Created by Christopher Moore on 2/22/19.
+//  Copyright © 2019 Christopher Moore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
